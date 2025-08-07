e5605643eb20047efd9a8ffdbdbb4b5bc8654ea0
gaeunpark7 <hen77360@gmail.com>
2025-08-08 02:35:30 +0900

    캘린더

---
+++ lib/features/calender/model/daily_stat_model.dart
@@ -0,0 +1,28 @@
+class DailyStat {
+  final String userId;
+  final DateTime day;
+  final int totalIncome;
+  final int totalExpense;
+  final int cashExpense;
+  final int cardExpense;
+
+  DailyStat({
+    required this.userId,
+    required this.day,
+    required this.totalIncome,
+    required this.totalExpense,
+    required this.cashExpense,
+    required this.cardExpense,
+  });
+
+  factory DailyStat.fromMap(Map<String, dynamic> map) {
+    return DailyStat(
+      userId: map['user_id'],
+      day: DateTime.parse(map['day']),
+      totalIncome: map['total_income'] ?? 0,
+      totalExpense: map['total_expense'] ?? 0,
+      cashExpense: map['cash_expense'] ?? 0,
+      cardExpense: map['card_expense'] ?? 0,
+    );
+  }
+}
