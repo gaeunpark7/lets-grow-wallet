c4462cc87f8abb07a66c692f39866fd37e3e68ef
gaeunpark7 <hen77360@gmail.com>
2025-11-18 00:12:34 +0900

    캐릭터 도감 ui 수정

--- lib/features/account_book/character/widgets/character_book_list.dart
+++ lib/features/account_book/character/widgets/character_book_list.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:lets_grow_wallet/utils/colors.dart';
 
 class CharacterBookList extends StatefulWidget {
-  const CharacterBookList({super.key});
+  final Function(int) onCharacterSelected;
+
+  const CharacterBookList({super.key, required this.onCharacterSelected});
 
   @override
   State<CharacterBookList> createState() => _CharacterBookListState();
 }
 
 class _CharacterBookListState extends State<CharacterBookList> {
+  int seletedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,14 +26,23 @@ class _CharacterBookListState extends State<CharacterBookList> {
         ),
         itemCount: 6,
         itemBuilder: (context, index) {
-          return Container(
-            width: MediaQuery.of(context).size.width * 0.2,
-            height: MediaQuery.of(context).size.width * 0.4,
-            decoration: BoxDecoration(
-              color: MainColors.mainLight,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(100),
-                bottom: Radius.circular(100),
+          final isSeleted = index == seletedIndex;
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                seletedIndex = index;
+              });
+              widget.onCharacterSelected(index);
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.width * 0.4,
+              decoration: BoxDecoration(
+                color: isSeleted ? MainColors.mainLight : MainColors.main,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(100),
+                  bottom: Radius.circular(100),
+                ),
               ),
             ),
           );
--- lib/features/account_book/character/widgets/character_book_list_detail.dart
+++ lib/features/account_book/character/widgets/character_book_list_detail.dart
@@ -1,10 +1,15 @@
-import 'dart:nativewrappers/_internal/vm/lib/math_patch.dart';
-
 import 'package:flutter/material.dart';
+import 'package:lets_grow_wallet/features/account_book/character/model/character_model.dart';
 import 'package:lets_grow_wallet/utils/colors.dart';
 
 class CharacterBookListDetail extends StatefulWidget {
-  const CharacterBookListDetail({super.key});
+  final int selectedCharacterIndex;
+  final List<Character> characters;
+  const CharacterBookListDetail({
+    super.key,
+    required this.selectedCharacterIndex,
+    required this.characters,
+  });
 
   @override
   State<CharacterBookListDetail> createState() =>
@@ -12,64 +17,100 @@ class CharacterBookListDetail extends StatefulWidget {
 }
 
 class _CharacterBookListDetailState extends State<CharacterBookListDetail> {
+  int selectedCircleIndex = -1;
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        padding: EdgeInsets.all(12),
-        height: MediaQuery.of(context).size.height * 0.5,
-        width: MediaQuery.of(context).size.width * 1,
-        decoration: BoxDecoration(
-          border: Border.all(color: MainColors.mainLight),
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    final selectedCharacter = widget.characters[widget.selectedCharacterIndex];
+
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        border: Border.all(color: MainColors.mainLight),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(5, (i) {
+              if (i.isEven) {
+                final index = i ~/ 2;
+                final isSelected = selectedCircleIndex == index;
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedCircleIndex = index;
+                    });
+                  },
+                  child: ClipOval(
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      color: isSelected
+                          ? MainColors.mainLight
+                          : MainColors.main,
+                    ),
+                  ),
+                );
+              } else {
+                // 화살표 아이콘
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Icon(
+                    Icons.arrow_forward,
+                    color: MainColors.mainDark,
+                    size: 28,
+                  ),
+                );
+              }
+            }),
+          ),
+          const SizedBox(height: 12),
+          ClipOval(
+            child: Container(width: 100, height: 100, color: MainColors.main),
+          ),
+          const SizedBox(height: 12),
+          Container(
+            height: 40,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              border: Border.all(color: MainColors.mainLight),
+            ),
+            child: Row(
               children: [
-                _buildCharacterDetail(),
-                SizedBox(width: 12),
-                Icon(Icons.arrow_forward, color: MainColors.mainDark),
-                SizedBox(width: 12),
-                _buildCharacterDetail(),
-                SizedBox(width: 12),
-                Icon(Icons.arrow_forward, color: MainColors.mainDark),
-                SizedBox(width: 12),
-                _buildCharacterDetail(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    selectedCharacter.name,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: MainColors.mainDark,
+                    ),
+                  ),
+                ),
               ],
             ),
-            ClipOval(
-              child: Container(width: 100, height: 100, color: MainColors.main),
+          ),
+          const SizedBox(height: 12),
+          Container(
+            height: 80,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              border: Border.all(color: MainColors.mainLight),
             ),
-            ClipOval(
-              child: Container(width: 100, height: 100, color: MainColors.main),
-            ),
-            SizedBox(height: 12),
-            Container(
-              height: 40,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                border: Border.all(color: MainColors.mainLight),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                selectedCharacter.description,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: MainColors.mainDark,
+                ),
               ),
             ),
-            SizedBox(height: 12),
-            Container(
-              height: 80, // SizedBox(height: 24),
-
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                border: Border.all(color: MainColors.mainLight),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-    );
-  }
-
-  _buildCharacterDetail() {
-    return ClipOval(
-      child: Container(width: 60, height: 60, color: MainColors.mainLight),
     );
   }
 }
